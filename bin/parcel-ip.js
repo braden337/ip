ff7f50cf997513070d6d1067f07e36c49c097c0d
Braden Jonsson <braden337@gmail.com>
2020-09-24 12:48:18 -0500

    Display qrcode of url

--- bin/parcel-ip.js
+++ bin/parcel-ip.js
@@ -2,10 +2,13 @@
 
 let address = require("address");
 let chalk = require("chalk");
+let qrcode = require("qrcode-terminal");
 
 let ipv4 = address.ip();
 let port = 1234;
 
 let url = `http://${ipv4}:${port}`;
 
-console.log(chalk.bold("Server running at", chalk.cyan(url)));
+qrcode.generate(url, { small: true }, function (qrcode) {
+  console.log(qrcode + chalk.bold("Server running at", chalk.cyan(url)));
+});
